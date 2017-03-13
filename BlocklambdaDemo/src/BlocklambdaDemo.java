8e811cee8a4cec5d1a847997e7956e2772514738
王珏 <jue952@live.cn>
2017-03-13 11:16:43 +0800

    Create project BlocklambdaDemo

---
+++ BlocklambdaDemo/src/BlocklambdaDemo.java
@@ -0,0 +1,23 @@
+interface NumericFunc {
+	int func(int n);
+}
+
+public class BlocklambdaDemo {
+	public static void main(String[] args) {
+		NumericFunc smallestF = (n) -> {
+			int result = 1;
+			n = n < 0 ? -n : n;
+			for (int i = 2; i < n / i; i++) {
+				if ((n % i) == 0) {
+					result = i;
+					break;
+				}
+			}
+
+			return result;
+		};
+
+		System.out.println("Smallest factor of 12 is " + smallestF.func(12));
+		System.out.println("Smallest factor of 11 is " + smallestF.func(11));
+	}
+}
