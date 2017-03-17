1e5ab5084ed92e484040087f4439963ab9e4275e
minijue <jue952@126.com>
2017-03-17 20:03:27 +0800

    1）增加 display 函数 2）增加 main 函数应用类

---
+++ ClassTest/test.cpp
@@ -0,0 +1,18 @@
+/*
+ * test.cpp
+
+ *
+ *  Created on: 2017年3月17日
+ *      Author: wangjue
+ */
+#include "Student.h"
+
+int main() {
+	Student s1{"zhang3", 72.0, 79.5};
+	s1.dispaly();
+
+	return 0;
+}
+
+
+
