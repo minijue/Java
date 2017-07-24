9b0326a0e1824c26b426f0fcb25a4cea266469a1
minijue <jue952@126.com>
2017-07-24 14:37:43 +0800

    创建项目 MultiThreadServer，作为 Android 项目 MultiThreadClient 的服务器端

---
+++ MultiThreadServer/src/ServerThread.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.Socket;
+
+public class ServerThread implements Runnable {
+	Socket s = null;
+	BufferedReader br = null;
+
+	public ServerThread(Socket s) throws UnsupportedEncodingException, IOException {
+		this.s = s;
+		br = new BufferedReader(new InputStreamReader(s.getInputStream(), "utf-8"));
+	}
+
+	@Override
+	public void run() {
+		String content = null;
+		while ((content = readFromClient()) != null) {
+			for (Socket s : MyServer.socketList) {
+				OutputStream os;
+				try {
+					os = s.getOutputStream();
+					os.write((content + "\n").getBytes("utf-8"));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	private String readFromClient() {
+		try {
+			return br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+			MyServer.socketList.remove(s);
+		}
+		return null;
+	}
+
+}
