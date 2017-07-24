981cb3aea8e6d6b175e9bd2199dc514839f0d557
minijue <jue952@126.com>
2017-07-24 11:26:39 +0800

    服务器代码

--- SimpleServer/src/SimpleServer.java
+++ SimpleServer/src/SimpleServer.java
@@ -1,9 +1,21 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class SimpleServer {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		@SuppressWarnings("resource")
+		ServerSocket ss = new ServerSocket(30000);
+		while (true) {
+			Socket s=ss.accept();
+			OutputStream os=s.getOutputStream();
+			os.write("来自服务器的回复\n".getBytes("utf-8"));
+			os.close();
+			s.close();
+		}
 	}
 
 }
