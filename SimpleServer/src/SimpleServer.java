a9e3bdb499526a4ef5bde47e0fff752a4885c5c0
minijue <jue952@126.com>
2017-07-24 12:32:56 +0800

    增加调试信息

--- SimpleServer/src/SimpleServer.java
+++ SimpleServer/src/SimpleServer.java
@@ -9,9 +9,11 @@ public class SimpleServer {
 		// TODO Auto-generated method stub
 		@SuppressWarnings("resource")
 		ServerSocket ss = new ServerSocket(30000);
+		System.out.println(ss);
 		while (true) {
-			Socket s=ss.accept();
-			OutputStream os=s.getOutputStream();
+			Socket s = ss.accept();
+			OutputStream os = s.getOutputStream();
+			System.out.println("Running...");
 			os.write("来自服务器的回复\n".getBytes("utf-8"));
 			os.close();
 			s.close();
