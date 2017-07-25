bafd9afe9a961cc743be118a3af1ac603a19fffc
minijue <jue952@126.com>
2017-07-25 10:20:23 +0800

    配合Android网络开发用的Servlet

---
+++ abc/src/wangjue/login.java
@@ -0,0 +1,54 @@
+package wangjue;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		if (request.getParameter("user") == null || request.getParameter("pwd") == null) {
+			response.getOutputStream().write("参数为空!".getBytes("utf-8"));
+			return;
+		}
+
+		if (!request.getParameter("user").equals("wangjue") || !request.getParameter("pwd").equals("www")) {
+			response.getOutputStream().write("登录失败!".getBytes("utf-8"));
+		} else {
+			response.getWriter().append("登录成功!");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
