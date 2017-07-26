a8d350ba3ad821b0fb55f607a606dfcf76b18011
minijue <jue952@126.com>
2017-07-26 19:35:49 +0800

    加入Filter类

---
+++ abc/src/wangjue/testFilter.java
@@ -0,0 +1,41 @@
+package wangjue;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class testFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpSession session = ((HttpServletRequest) request).getSession();
+		String user = (String) session.getAttribute("username");
+		String pwd = (String) session.getAttribute("password");
+		if ((user == null || pwd == null) || (!user.equals("wangjue") || !pwd.equals("www"))) {
+			((HttpServletResponse) response).sendRedirect("notfound.html");
+		}
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
