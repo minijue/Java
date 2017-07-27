9444204578735a234125af94636b54c17876f592
minijue <jue952@126.com>
2017-07-27 16:10:46 +0800

    修改发布地址

--- helloWorld/src/com/wangjue/webServiceApp.java
+++ helloWorld/src/com/wangjue/webServiceApp.java
@@ -6,7 +6,7 @@ public class webServiceApp {
 	public static void main(String[] args) {
 		System.out.println("web service start");
 		HelloWorldImpl implementor = new HelloWorldImpl();
-		String address = "http://localhost:8080/helloWorld";
+		String address = "http://192.168.7.9:8080/helloWorld";
 		Endpoint.publish(address, implementor);
 		System.out.println("web service started");
 	}
